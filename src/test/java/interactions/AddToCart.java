758a4b9ec37f9b4c948ce64106a1a3542cd3a7b6
davidcardenasdis <david_cardenas@discovery.com>
2023-06-30 16:57:53 -0500

    Target implementation

--- src/test/java/interactions/AddToCart.java
+++ src/test/java/interactions/AddToCart.java
@@ -3,17 +3,14 @@ package interactions;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.ui.PageElement;
+import net.serenitybdd.screenplay.ui.Button;
 import org.openqa.selenium.By;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 public class AddToCart {
 
     public static Performable item(String name){
         return Task.where("{0} adds " + name + " to the cart",
-                Click.on(PageElement.located(By.cssSelector("button[id*='" + name + "']")))
+                Click.on(Button.located(By.cssSelector("button[id*='" + name + "']")))
                 );
     }
 
--- src/test/java/interactions/NavigateFromHomePage.java
+++ src/test/java/interactions/NavigateFromHomePage.java
@@ -3,13 +3,17 @@ package interactions;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.targets.Target;
 import net.serenitybdd.screenplay.ui.PageElement;
+import org.openqa.selenium.By;
 
 public class NavigateFromHomePage {
 
+    private static final Target CART_ICON = Target.the("shopping cart icon").located(By.id("shopping_cart_container"));
+
     public static Performable toCartPage() {
         return Task.where("{0} navigates to cart page",
-                Click.on(PageElement.withNameOrId("shopping_cart_container"))
+                Click.on(CART_ICON)
         );
     }
 }
